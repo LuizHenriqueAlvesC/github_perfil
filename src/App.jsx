866a01fd52657d565fc5cd67ab082b9a275103c3
luiz <luiz980alves@gmail.com>
2024-07-14 21:43:32 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Perfil from "./Components/Perfil/index";
+import Formulario from "./Components/Formulario";
+import { useState } from "react";
+import ReposList from "./Components/ReposList";
+
+
+
+function App() {
+  const [forumlarioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+
+  return(
+    <>
+    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}/>
+
+    {nomeUsuario.length > 4 && (
+      <>
+      <Perfil nomeUsuario={nomeUsuario} />
+      <ReposList nomeUsuario={nomeUsuario} />
+      </>
+    )}
+
+    {/* {forumlarioEstaVisivel && (
+      <Formulario />
+    )}
+
+    <button onClick={() => setFormularioEstaVisivel(!forumlarioEstaVisivel)} type="button">toggle form</button> */}
+    </>
+  )
+}
+
+export default App
